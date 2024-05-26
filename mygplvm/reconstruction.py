6286e83830b25ffc64a1dd5f18ae9e0f6d4cfafe
Bryan Amirul Husna <bryanahusna@gmail.com>
2024-05-26 17:50:16 +0700

    feat: vehicle color

--- mygplvm/reconstruction.py
+++ mygplvm/reconstruction.py
@@ -2,19 +2,20 @@ import numpy as np
 from scipy.fftpack import dct, idct
 import skimage
 import trimesh
+from math import sin, cos, tan, radians
 
 
 from mygplvm.mygplvm import MyGPLVM
 
 class Reconstruction():
-    def __init__(self, voxel_resolution = 60, dct_resolution = 25) -> None:
+    def __init__(self, voxel_resolution = 64, dct_resolution = 25) -> None:
         self.dct_resolution = dct_resolution
         self.voxel_resolution = voxel_resolution
 
     def fit_from_obj(self):
         pass
 
-    def fit_from_sdf(self, sdfs, voxel_resolution=60):
+    def fit_from_sdf(self, sdfs, voxel_resolution=64):
         self.voxel_resolution = voxel_resolution
         dct_trimmeds = []
         for sdf in sdfs:
@@ -25,7 +26,7 @@ class Reconstruction():
         dct_flatteneds = []
         for dct_trimmed in dct_trimmeds:
             dct_flatteneds.append(dct_trimmed.flatten())
-        self.gplvm.simple_gplvm(dct_flatteneds, 'vehicle_models', latent_dimension = 2, epsilon = 0.001, maxiter = 50)
+        return self.gplvm.simple_gplvm(dct_flatteneds, 'vehicle_models', latent_dimension = 2, epsilon = 0.001, maxiter = 50)
 
     def reconstruct_from_x(self, x):
         dct_n_flat = self.gplvm.recall(x)
@@ -33,10 +34,17 @@ class Reconstruction():
         dct_full = np.pad(dct_n, ((0, self.voxel_resolution - self.dct_resolution), (0, self.voxel_resolution - self.dct_resolution), (0, self.voxel_resolution - self.dct_resolution)))
         
         voxels_idct = idctn(dct_full)
+        voxels_idct = np.pad(voxels_idct, ((1, 1), (1, 1), (1, 1)), constant_values=(1, 1))
         mc_vertices, mc_faces, mc_normals, _ = skimage.measure.marching_cubes(voxels_idct, level=0)
         mc_mesh = trimesh.Trimesh(vertices=mc_vertices, faces=mc_faces, normals=mc_normals)
         translate_offset = -(self.voxel_resolution - 4) // 2
         mc_mesh.apply_translation([translate_offset, translate_offset, translate_offset])
+        mc_mesh.apply_transform([
+            [cos(radians(90))  , 0, sin(radians(90))  , 0],
+            [0                      , 1, 0                      , 0],
+            [-sin(radians(90)) , 0, cos(radians(90))  , 0],
+            [0                      , 0, 0                      , 1]
+        ])
         return mc_mesh
 
 def dctn(x, norm="ortho"):
